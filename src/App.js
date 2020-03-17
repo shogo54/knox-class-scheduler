4157f356830c4be0fcf2fff31dcb736e74742516
shogo54 <sakiyama@knox.edu>
2020-03-16 22:01:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 import './App.css';
 import SubjectList from './components/SubjectList';
+import CurrentCourseList from './components/CurrentCourseList';
+import MenuAppBar from './components/MenuAppBar';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: '#8FB4FF',
+  }
+}));
 
 export default function App() {
+
+  const classes = useStyles();
+
   return (
-    <Container>
-      <SubjectList />
-    </Container>
+    <Box className={classes.root} >
+      <MenuAppBar />
+      <Grid container justify='center'>
+        <SubjectList />
+        <CurrentCourseList />
+      </Grid>
+    </Box>
   );
 }
